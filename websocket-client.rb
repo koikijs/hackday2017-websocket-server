c4b44f299a8db325cc18b91e1fc5549277134c46
nabuchi <nabnab.main+github@gmail.com>
2017-02-04 16:59:34 +0900

    add websocket implementation

---
+++ websocket-client.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'websocket-client-simple'
+
+ws = WebSocket::Client::Simple.connect 'http://localhost:8888'
+
+ws.on :message do |msg|
+  puts msg.data
+end
+
+ws.on :open do
+  ws.send 'hello!!!'
+end
+
+ws.on :close do |e|
+  p e
+  exit 1
+end
+
+#loop do
+  #ws.send STDIN.gets.strip
+#end
+sleep 1
